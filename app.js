18cf6dfd932541e9cd6309c27f1ce35879376cb0
Abhishek Jachak <abhishekjachak22@gmail.com>
2024-04-05 13:29:10 +0530

    y

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors({
 // app.use(express.urlencoded({extended: true, limit: "16kb"}))
 // app.use(express.static("public"))
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 
 //routes import
